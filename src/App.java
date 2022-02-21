e76bff143ce5c71b8b681daf713a2c35fd84769a
raphaelcunhas <fraphaelcs@gmail.com>
2022-02-21 14:27:24 -0300

    tentando adicionar menu

--- src/App.java
+++ src/App.java
@@ -1,14 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        /** cria o cliente
         Cliente raphael = new Cliente();
         raphael.setNome("Raphel Cunha");
+        cria conda p/ cliente
         Conta cc = new ContaCorrente(raphael);
         Conta poupanca = new ContaPoupanca(raphael);
-        
-        //cc.depositar(100);
-        cc.transferir(100, poupanca);
+        */ 
+        Banco.menuAdm();
 
-        cc.imprimirExtrato();
-        poupanca.imprimirExtrato();
+        //cc.depositar(100);
+        //cc.transferir(100, poupanca);
+
+        //cc.imprimirExtrato();
+        //poupanca.imprimirExtrato();
     }
 }
